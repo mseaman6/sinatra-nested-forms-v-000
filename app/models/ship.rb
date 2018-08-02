a3ad60ed31ebb8c9c80614c088fcd44b0e94de79
Monica Seaman <monica.l.seaman@gmail.com>
2018-08-02 16:39:49 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,17 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  SHIPS = []
+
+  def initialize(params)
+    @name = params
+  end
+
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
 end
